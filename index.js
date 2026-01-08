3d24786611c3816e9689a824d8f46ebe87d0fac9
pooja221133 <pooja.hunasnale98@gmail.com>
2026-01-08 07:00:49 +0530

    fixed error

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log('change from main');
-console.log("hello")
+console.log("hello33333333")
 function hello()
 {
     console.log("good")
+}
+function test()
+{
+    console.log("test")
 }
